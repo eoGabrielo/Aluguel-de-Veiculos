b99087cc725a86d215259c247c4fea2bbe9a87f1
Gabriel Oliveira <105285297+eoGabrielo@users.noreply.github.com>
2022-10-19 11:07:01 -0300

    Adicionado While
    Agora o programa pede pra escolher entre moto ou carro até uma das opções forem escolhidas

--- Aluguel de veiculo.py
+++ Aluguel de veiculo.py
@@ -1,36 +1,34 @@
 
-print(" ") # STYLE
+print("-="* 100)
 print("ALUGUEL DE VEICULOS")
-print(" ") # STYLE
+print("-="* 100)
 
-tipoDoVeiculo= str(input("O veiculo é carro ou moto? ")) # Dependendo da escolha o valor por KM é alterado.
+tipoDoVeiculo= str(input("O veiculo é carro ou moto? [C/M]")).upper().strip()
 
-tipoDoVeiculoConta= 0 #Para fazer o calculo P/Km, começa valendo 0.
+while tipoDoVeiculo not in "CM":
+    tipoDoVeiculo = str(input("Somente Carro ou Moto, Tente novamente: [C/M] ")).upper().strip()
 
-if tipoDoVeiculo == "moto": #Se for moto tipoDoVeiculoConta recebe 0.15 P/KM;
+tipoDoVeiculoConta= 0
+
+if tipoDoVeiculo == "M":
     tipoDoVeiculoConta= 0.15
-elif tipoDoVeiculo == "carro":
-    tipoDoVeiculoConta= 2.15 #Se for carro tipoDoVeiculoConta recebe 2.15 P/KM;
-else: #Se for escrito algo sem ser carro ou moto, informa esse erro.
-    print(" ")
-    print(f"ERRO:.!!! {tipoDoVeiculo} !!! Não é uma opção, escolha somente carro ou moto!")
-    print("REINICIE O PROGRAMA...")
-    print(" ")
+if tipoDoVeiculo == "C":
+    tipoDoVeiculoConta= 2.15
 
 
-diasAlugado= int(input("Quantos dias foi alugado o veiculo? ")) 
+diasAlugado= int(input("Quantos dias foi alugado o veiculo? "))
 kmRodados= int(input("Quantos km o veículo rodou? "))
 diariaDoVeiculo= int(input("Quanto é a diaria do veiculo? "))
 
 
-diasAlugadoConta= diasAlugado * diariaDoVeiculo #Calcula os dias alugados pelo o valor escolhido;
-kmRodadosConta= kmRodados * tipoDoVeiculoConta#Calcula Km rodados por quanto vale o Km (Moto 0.15 P/Km, Carro 2.15 P/Km);
-total= diasAlugadoConta + kmRodadosConta #Soma o valores de Km e diaria.
+diasAlugadoConta= diasAlugado * diariaDoVeiculo
+kmRodadosConta= kmRodados * tipoDoVeiculoConta
+total= diasAlugadoConta + kmRodadosConta
 
-print(" ")
+print("-="* 100)
 print(f"Você rodou {kmRodados}Km e ficou {diasAlugado} dias com o veiculo;")
 print(f"Tipo do veiculo: {tipoDoVeiculo}")
 print(f"P/Dia: R${diariaDoVeiculo}")
 print(f"P/Km: R${tipoDoVeiculoConta}")
 print(f"Total a pagar: R${total:.2f}")
-print(" ")
+print("-="* 100)
